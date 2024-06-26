4ced2db553f711a18744c198e926c725b5da23cc
Jaskaran Singh <jass2k23@gmail.com>
2024-06-27 00:17:43 +0530

    Setting up my App | Config. Prismic & Slice

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,36 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+          <h1
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
+            aria-label={
+              slice.primary.first_name + " " + slice.primary.last_name
+            }
+          >
+            <span className="block text-slate-300">{slice.primary.first_name}</span>
+            <span>{slice.primary.last_name}</span>
+            <span>{slice.primary.tag_line}</span>
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
